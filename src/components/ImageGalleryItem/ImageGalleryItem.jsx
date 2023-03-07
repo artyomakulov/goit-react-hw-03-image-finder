fdc57e0fb04167e275ec234e6d38488a197593bf
artyomakulov <88495795+artyomakulov@users.noreply.github.com>
2023-03-07 16:28:16 +0200

    Update ImageGalleryItem.jsx
    Обновил с апи

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -21,15 +21,16 @@ export default class ImageGalleryItem extends Component {
   
   state = {
     hits: [],
-    isLoading: false,
+
     error: null,
+    status: 'idle'
   }
   componentDidUpdate(prevProps, prevState) {
     const prevName = prevProps.searchName;
     const nextName = this.props.searchName
     if (prevName !== nextName) {
       console.log("изменилось имя поиска")
-      this.setState({isLoading: true})
+      this.setState({status: 'pending'})
       fetch(`https://pixabay.com/api/?q=${nextName}&page=1&key=25323007-3d609b483f4fcb74c4bf2a361&image_type=photo&orientation=horizontal&per_page=12`)
       .then(response => {
         if (response.ok) {
@@ -37,31 +38,54 @@ export default class ImageGalleryItem extends Component {
         }
         return Promise.reject(new Error(`Нет такой картинки`));
       } )
-      .then(hits => this.setState({ ...hits }))
-      .catch(error => this.setState({ error }))
-      .finally(() => this.setState({ isLoading: false }))
+      .then(hits => this.setState({ ...hits, status: 'resolved' }))
+      .catch(error => this.setState({ error, status: 'rejected' }))
+      
     }
   }
 
 
   render() {
-    const {hits, isLoading, error} = this.state
-    return (
-    <div>
-     <ul>
-     {error && <h1>{error.message}</h1>}
-    {isLoading && <div>загружаем</div>}
-    {hits.map(({ id, webformatURL, tags }) => {
-      return (
-       
-          <li key={id}>
-      <img width='250px' height='250px' src={webformatURL} alt={tags}/>
-      </li>
-      
-      )
-    })}
-    </ul>
-    </div>
-    )
+    const {hits, error, status} = this.state;
+
+    if (status === 'idle') {
+      return <div>введите имя</div>
+    }
+    if (status === 'prending') {
+      return <div>загружаем</div>
+    }
+    if (status === 'rejected') {
+      return <h1>{error.message}</h1>
+    }
+    if (status === 'resolved') {
+      return <div>
+      <ul>
+      {hits.map(({...hits}) => {
+        return (
+          <li key={hits.id}>
+          <img width='250px' height='250px' src={hits.webformatURL} alt={hits.tags}/>
+          </li>
+        )
+      })}
+      </ul>  
+      </div>
+    
+  
+    }
   }
 }
+
+
+// {hits.map(({ id, webformatURL, tags }) => {
+//   return (
+   
+//       <li key={id}>
+//   <img width='250px' height='250px' src={webformatURL} alt={tags}/>
+//   </li>
+  
+//   )
+// })}
+// </ul>
+// </div>
+// )
+// }
